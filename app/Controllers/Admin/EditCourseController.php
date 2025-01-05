85d369292db9497e9c0cd032b6eaa6b1b68472ee
kayla1410 <130771373+kayla1410@users.noreply.github.com>
2025-01-05 13:57:27 +0700

    admin course

--- app/Controllers/Admin/EditCourseController.php
+++ app/Controllers/Admin/EditCourseController.php
@@ -10,6 +10,7 @@ class EditCourseController extends Controller
     public function index()
     {
         $model = new CoursesModel();
+        
         $data['courses'] = $model->findAll();
 
         return view('courses/index', $data);
@@ -32,7 +33,7 @@ class EditCourseController extends Controller
 
         $model->save($data);
 
-        return redirect()->to('/courses');
+        return redirect()->to('/admin/courses');
     }
 
     public function delete($id)
@@ -40,6 +41,6 @@ class EditCourseController extends Controller
         $model = new CoursesModel();
         $model->delete($id);
 
-        return redirect()->to('/courses');
+        return redirect()->to('/admin/courses');
     }
 }
